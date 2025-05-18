ae59a9f642f66b90a7a6c240c52e370887465fe4
ahmadremaw <mahmoudleen2@gmail.com>
2025-05-19 01:31:36 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    word_count = None
+    if request.method == "POST":
+        text = request.form["text"]
+        word_count = len(text.split())
+    return render_template("index.html", word_count=word_count)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
