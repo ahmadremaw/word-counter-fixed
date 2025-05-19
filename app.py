301938cba37e6ba7f8ba27aee089583403598fbc
ahmadremaw <mahmoudleen2@gmail.com>
2025-05-20 00:33:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,10 @@
-from flask import Flask, render_template, request
-from langdetect import detect
-from spellchecker import SpellChecker
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
 def index():
-    word_count = None
-    char_count = None
-    language = None
-    misspelled = []
-    text = ""
+    return render_template('index.html')
 
-    if request.method == "POST":
-        text = request.form.get("text", "")
-        word_count = len(text.split())
-        char_count = len([ch for ch in text if ch.strip() != ""])
-        try:
-            language = detect(text)
-        except:
-            language = "غير معروف"
-
-        if language == "en":
-            spell = SpellChecker()
-            misspelled = spell.unknown(text.split())
-
-    return render_template(
-        "index.html",
-        word_count=word_count,
-        char_count=char_count,
-        language=language,
-        text=text,
-        misspelled=misspelled
-    )
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
