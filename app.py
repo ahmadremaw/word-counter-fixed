3871b653afbc2ccb847a780008b7adbe85c3b24e
ahmadremaw <mahmoudleen2@gmail.com>
2025-05-20 01:36:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from langdetect import detect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    text = ""
+    word_count = char_count = language = None
+    if request.method == "POST":
+        text = request.form["text"]
+        word_count = len(text.split())
+        char_count = len(text)
+        try:
+            language = detect(text)
+        except:
+            language = "غير معروفة"
+    return render_template("index.html", text=text, word_count=word_count, char_count=char_count, language=language)
 
-if __name__ == '__main__':
+@app.route("/about")
+def about():
+    return render_template("pages/about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("pages/privacy.html")
+
+@app.route("/terms")
+def terms():
+    return render_template("pages/terms.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
