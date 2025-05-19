0b7e9b13490cb4920296bd5db66ecaaee9faa81c
ahmadremaw <mahmoudleen2@gmail.com>
2025-05-19 11:09:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,21 +14,26 @@ def index():
     text = ""
 
     if request.method == "POST":
-        text = request.form["text"]
+        text = request.form.get("text", "")
         word_count = len(text.split())
-        char_count = len(text.replace(" ", ""))
+        char_count = len([ch for ch in text if ch.strip() != ""])
         try:
             language = detect(text)
         except:
             language = "غير معروف"
 
-        # التدقيق الإملائي للإنجليزية فقط حالياً
         if language == "en":
             spell = SpellChecker()
             misspelled = spell.unknown(text.split())
 
-    return render_template("index.html", word_count=word_count, char_count=char_count,
-                           language=language, text=text, misspelled=misspelled)
+    return render_template(
+        "index.html",
+        word_count=word_count,
+        char_count=char_count,
+        language=language,
+        text=text,
+        misspelled=misspelled
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
